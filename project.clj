d20eb2bbd61b10b1b7e4e606b97c998df99b975f
Mike Vollmer <vollmerm@athena.ecs.csus.edu>
2012-08-04 08:06:46 -0700

    updated project.clj for codecache param, added basic unit tests

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject fungp "0.1"
   :description "Genetic programming in Clojure"
-  :aot :all
+  :aot [fungp.core]
+  :jvm-opts ["-XX:ReservedCodeCacheSize=128m" "-server"]
   :dev-dependencies [[lein-marginalia "0.7.0"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.numeric-tower "0.0.1"]]
