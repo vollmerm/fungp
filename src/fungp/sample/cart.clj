6741805103fff3feed40ef51d777a6cda8bdd32a
Mike Vollmer <mike@recurial.com>
2012-08-30 00:13:58 -0700

    started implementing mutable variables, moved sample files to new namespace

---
+++ src/fungp/sample/cart.clj
@@ -0,0 +1,90 @@
+;;; Sample: Cart problem
+;;; --------------------
+
+
+(ns fungp.sample.cart
+  "Cart problem example from Koza"
+  (:use fungp.core)
+  (:use fungp.util)
+  (:require clojure.pprint))
+
+;;; some constants
+
+(def MAX_T "Max time limit" 5)
+(def TEST_POINTS "Number of test cases" 20)
+
+(defn move-cart
+  "Simulate cart movement with the evolved function used to calculate
+   thrust. Assuming acceleration is one unit/second^2, and time is in
+   steps of 0.02."
+  [t v x f]
+  (loop [t (float t) v (float v) x (float x)]
+    (cond (and (<= (Math/abs x) 0.01) (<= (Math/abs v) 0.01)) t
+          (>= t MAX_T) 100
+          :else
+          (let [thrust (if (> (f v x) 0) 1.0 -1.0)]
+            (recur (+ t 0.02)
+                   (+ v (* thrust 0.02))
+                   (+ x (* v 0.02)))))))
+
+(defn rand-spread
+  "Range of starting position or velocity"
+  [l] (repeatedly l #(- (* 1.5 (rand)) 0.75)))
+
+(def cart-functions "Functions available to the evolved programs"
+  '[[+ 2][* 2][- 2][fungp.util/abs 1][fungp.util/gt 2]])
+
+(def cart-terminals "Terminals available to the evolved programs"
+  '[x v -1])
+
+(def rand-velocity (rand-spread TEST_POINTS))
+(def rand-position (rand-spread TEST_POINTS))
+
+(defn cart-error
+  "Calculate error using move-cart"
+  [f]
+  (reduce + (map (fn [v x] (move-cart 0 v x f))
+                 rand-velocity
+                 rand-position)))
+
+;;; some existing solutions
+
+(defn cart-optimal "The optimal solution" [v x] (gt (* -1 x) (* v (abs v))))
+
+(defn cart-suboptimal [v x] (gt (* -1 x) (* v v)))
+
+(def cart-optimal-error (cart-error cart-optimal))
+
+(def cart-evol (fn [v x] (let [] (fungp.util/gt (- x v) (+ (+ x x) (+ x x))))))
+(def cart-evol2 (fn [v x] (let [] (- (- (- -1 x) (+ x v))
+                                     (- (* (+ -1 v) (* -1 x))
+                                        (fungp.util/gt (fungp.util/abs x) -1))))))
+
+(defn cart-fitness
+  "Compute fitness for cart problem."
+  [tree] (let [f (eval (list 'fn '[v x] tree))]
+           (cart-error f)))
+
+(defn cart-report
+  "Reporting function. Prints out the tree and its score"
+  [tree fitness]
+  (clojure.pprint/pprint (list 'fn '[v x] tree))(print "\n")
+  (print "Error:\t")(print fitness)(print "\n\n")
+  (flush))
+
+(defn test-cart
+  "Run the cart problem"
+  [n1 n2]
+  (println "Koza's Cart Problem")
+  (println "Error is number of seconds taken total across all the tests.")
+  (println "If a program times out it is assigned 100 seconds.")
+  (let [options {:iterations n1 :migrations n2 :num-islands 4
+                 :population-size 100 :terminals cart-terminals
+                 :tournament-size 3
+                 :functions cart-functions :fitness cart-fitness
+                 :report cart-report
+                 :adf-count 1
+                 :max-depth 5}
+        [tree score] (rest (run-genetic-programming options))]
+    (do (println "Done!")
+        (cart-report tree score))))
---
+++ src/fungp/sample/sinbowl.clj
@@ -0,0 +1,59 @@
+(ns fungp.sample.sinbowl
+  (:use fungp.core)
+  (:use fungp.util)
+  (:require clojure.pprint))
+
+(defn sinbowl-function
+  "The function to match"
+  [x] (- (* 0.1 (abs x)) (sin x)))
+
+
+(def sinbowl-functions
+  "Functions and their arities"
+  '[[+ 2][- 2][* 2][fungp.util/abs 1]
+    [Math/sin 1][fungp.util/sdiv 2][inc 1][dec 1]])
+
+(def sinbowl-parameters
+  "Only one parameter for sinbowl"
+  '[x])
+
+(def sinbowl-numbers
+  "Lots of number literals to use as terminals"
+  '[-1 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1 10])
+
+(def test-range
+  "The input test cases"
+  (map #(- % 20) (range 40)))
+
+(def sinbowl-actual
+  "The output test cases"
+  (map sinbowl-function test-range))
+
+(defn sinbowl-fitness
+  "Fitness function. Takes a tree, evals it, and returns a fitness/error score."
+  [tree]
+  (try
+    (let [f (eval (list 'fn 'testfunc '[x] tree))
+          results (map f test-range)]
+      (reduce + (map off-by-sq sinbowl-actual results)))
+    (catch Exception e (println e) (println tree))))
+
+(defn sinbowl-report
+  "Reporting function. Prints out the tree and its score"
+  [tree fitness]
+  (clojure.pprint/pprint (list 'fn '[a] tree))(print "\n")
+  (print "Error:\t")(print fitness)(print "\n\n")
+  (flush))
+
+(defn test-sinbowl [n1 n2]
+  (println (str "Test inputs: " (vec test-range)))
+  (println (str "Test outputs: " (vec sinbowl-actual)))
+  (println (str "Max generations: " (* n1 n2)))
+  (println)
+  (let [options {:iterations n1 :migrations n2 :num-islands 6 :population-size 100 :tournament-size 5 :mutation-probability 0.1
+                 :max-depth 10 :terminals sinbowl-parameters :numbers sinbowl-numbers :fitness sinbowl-fitness
+                 :functions sinbowl-functions :report sinbowl-report}
+        [tree score] (rest (run-genetic-programming options))]
+    (do (println "Done!")
+        (sinbowl-report tree score))))
+  
