86f9ecdc279f30d66469bcb90101d67e16a872e2
Mike Vollmer <mike@talkingincircles.net>
2012-09-05 01:37:20 -0700

    started implementing java api, renamed sample to tutorial

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject fungp "0.2"
   :description "Genetic programming in Clojure"
   :jvm-opts ["-XX:ReservedCodeCacheSize=128m" "-server"]
-  :dev-dependencies [[lein-marginalia "0.7.0"]]
+  :dev-dependencies [[lein-marginalia "0.7.1"]]
   :dependencies [[org.clojure/clojure "1.3.0"]]
+  :aot [fungp.java-api fungp.core]
+  :java-source-paths ["src/fungp/java"]
   :plugins      [[lein-swank "1.4.4"]])
