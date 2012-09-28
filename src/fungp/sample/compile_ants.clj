7f49d0379f82d08f6374445a69c23acc47c300bd
Mike Vollmer <mike@recurial.com>
2012-09-28 14:56:26 -0700

    Updated compile-ants sample with more sophisticated fitness function and additional training data

--- src/fungp/sample/compile_ants.clj
+++ src/fungp/sample/compile_ants.clj
@@ -14,15 +14,29 @@
 
 ;;; First some constants need to be established.
 
-(def MAXSTEPS "Maximum number of iterations before penalty"
-  30)
+(def MAXSTEPS "Maximum number of steps before penalty"
+  50)
 
-(def STARTFOOD "Location of food to be eaten."
+(def PENALTY "Penalty applied to individuals that don't eat all the food"
+  50)
+
+(def MAXITER "Maximum number of iterations (length of loop in fitness function)"
+  50)
+
+(def STARTFOOD1 "Locations of food to be eaten."
   [[1 1][1 2][1 3][1 4][1 5][2 5][3 5][4 5][5 5][5 4]
-   [5 3][6 3][7 3][8 3][8 4][7 4][6 4][6 2][7 2][8 3]])
+   [5 3][6 3][7 3][8 3][8 4][7 4][6 4][6 2][7 2][8 3]
+   [8 4][9 4][9 5][9 6][9 8][8 8][8 7][9 7]])
 
-(def NUMFOOD "Number of food items."
-  (count STARTFOOD))
+(def STARTFOOD2 "Second set of locations of food to be eaten."
+  [[0 1][1 1][1 2][2 2][3 2][3 3][4 3][5 4][5 5][5 6]
+   [6 6][7 6][8 6][9 6][9 7][9 8][9 9][8 9][7 9][6 9]
+   [6 8][5 8][5 7][4 7][4 6][4 5][4 4][4 3]])
+
+(def TESTFOOD "New set of food to test for generalization"
+  [[1 0][2 0][3 0][4 0][5 0][6 1][6 2][6 3][6 4][6 5]
+   [6 6][5 6][4 6][4 7][3 7][2 7][1 7][0 7][0 6][0 5]
+   [1 5][2 5][2 4][2 3][2 1][3 1][3 2][3 3]])
 
 ;;; Vars in Clojure are thread-local, which is a useful property. Here
 ;;; a bunch of dynamic vars are declared, and they can be referenced by
@@ -93,6 +107,7 @@
 
 (defn move []
   (do
+    (set! steps (inc steps))
     (set! ant-x (new-x ant-dir ant-x))
     (set! ant-y (new-y ant-dir ant-y))
     (set! eaten (new-eaten ant-x ant-y food eaten))
@@ -101,21 +116,25 @@
 (def ant-terminals '[(left) (right) (move)])
 (def ant-functions '[[sense 2][do 2][do 3]])
 
-(defn ant-compile-fitness
+(defn run-ant
   "This evaluates the fitness of the individuals. It uses a binding block to create
-   thread-local bindings, evaluates the evolved code, and runs it in a loop."
-  [tree]
-  (binding [ant-x 0 ant-y 0 ant-dir 0 food STARTFOOD eaten 0 steps 0]
+  thread-local bindings, evaluates the evolved code, and runs it in a loop."
+  [tree init-food]
+  (binding [ant-x 0 ant-y 0 ant-dir 0 food init-food eaten 0 steps 0]
     (let [func (eval (list 'fn [] tree))]
-      (loop [n MAXSTEPS]
+      (loop [iter MAXITER]
         (do
           (func) ;; run the code
-          (set! steps (inc steps))
           (if (zero? (count food))
             steps
-            (if (zero? n)
-              (+ MAXSTEPS (count food))
-              (recur (- n 1)))))))))
+            (if (or (zero? iter) (zero? steps) (> steps MAXSTEPS))
+              (+ MAXSTEPS PENALTY (count food))
+              (recur (dec iter)))))))))
+
+
+(defn ant-fitness
+  "Tests the fitness of an ant using run-ant and STARTFOOD."
+  [tree] (+ (run-ant tree STARTFOOD1) (run-ant tree STARTFOOD2)))
 
 (defn ant-report
   "Reporting function. Prints out the tree and its score"
@@ -124,13 +143,17 @@
   (println (str "Error:\t" fitness "\n\n")))
 
 (defn test-compile-ants [n1 n2]
-  (println "Ant problem (compiled)\n")
-  (println (str "Food count: " NUMFOOD "\n"))
+  (println "Ant problem, from Koza")
+  (println (str "Generations: " (* n1 n2)))
+  (println "Training is done on two data sets, each with a penalty of 50 and 50 max steps.")
   (let [options {:iterations n1 :migrations n2 :num-islands 8
-                 :population-size 25 :max-depth 4
-                 :terminals ant-terminals :fitness ant-compile-fitness
+                 :tournament-size 3
+                 :population-size 20 :max-depth 5
+                 :terminals ant-terminals :fitness ant-fitness
                  :functions ant-functions :report ant-report}
         [tree score] (rest (run-genetic-programming options))]
     (do (println "Done!")
-        (ant-report tree score))))
+        (ant-report tree score)
+        (println "Testing for generalization (one data set, same penalty and max steps)")
+        (println (str "Score: " (run-ant tree TESTFOOD))))))
 
